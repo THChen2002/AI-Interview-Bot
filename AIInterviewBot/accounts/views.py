2337a49fd175c688c3cf2379da4d2c5e21c2d9b5
Sunny3939hum <funnysunny3939@gmail.com>
2023-09-12 18:02:03 +0800

    feature-PasswordComfirmComplete 1.新增密碼重設成功的頁面 2.新增使用者收信後的連結頁 3.新增view.py的功能密碼重設和密碼收信

--- AIInterviewBot/accounts/views.py
+++ AIInterviewBot/accounts/views.py
@@ -11,3 +11,32 @@ def index(request):
 def register(request):
     registerForm = RegisterForm()
     return render(request, 'accounts/register.html', locals())
+
+#使用者收信後的連結頁面
+def password_reset_confirm(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        # 顯示密碼重設表單
+        form = SetPasswordForm(user=user)
+        if request.method == 'POST':
+            form = SetPasswordForm(user=user, data=request.POST)
+            if form.is_valid():
+                form.save()
+                user.reset_password_token = None
+                user.save()
+                messages.success(request, '您的密碼已成功重設。')
+                return redirect('password_reset_complete')
+        return render(request, 'accounts/reset_password_confirm.html', {'form': form})
+    else:
+        # 顯示錯誤訊息
+        messages.error(request, '密碼重設連結無效或已過期。')
+        return redirect('forgot_password')
+    
+#密碼重設成功頁面
+def password_reset_complete(request):
+    return render(request, 'accounts/reset_password_complete.html')
